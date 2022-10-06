a8f2c0eedfd895a8f88e4187bd23f2b88f5bae0a
Ingrid Aquino <ingridaquinosoares@live.com>
2022-10-06 12:07:44 -0300

    :construction: Adicionados novos projetos

--- src/components/myprojects/Projects.tsx
+++ src/components/myprojects/Projects.tsx
@@ -6,6 +6,8 @@ import addRemove from '../../assets/addandremo.svg';
 import ballebot from '../../assets/ballebot.svg';
 import tabuleiro from '../../assets/tabuleiro.svg';
 import buscador from '../../assets/buscador.svg';
+import blogpessoal from '../../assets/blogpessoal.svg';
+import falamemo from '../../assets/falamemo.svg';
 
 
 import React, { Component } from "react";
@@ -14,6 +16,45 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { ComponentProject } from '../componentproject/ComponentProject';
 
+
+
+const  project = [
+  {
+    imagem: `${falamemo}`,
+    link: 'https://falamemo.netlify.app/',
+    projeto: 'FalaMemo'
+  },
+  {
+    imagem: `${blogpessoal}`,
+    link: 'https://blog-pessoal-react.netlify.app/',
+    projeto: 'Blog Pessoal'
+  },
+  {
+    imagem: `${squirtle}`,
+    link: 'https://squirtle-ong.netlify.app',
+    projeto: 'Squirtle'
+  },
+  {
+    imagem: `${bookstan}`,
+    link: 'https://book-stan.netlify.app/',
+    projeto: 'Bookstan'
+  },
+  {
+    imagem: `${addRemove}`,
+    link: 'https://github.com/Ingridaquino/add-remove',
+    projeto: 'ToDo List'
+  },
+  {
+    imagem: `${buscador}`,
+    link: 'https://frabjous-marzipan-5fc57e.netlify.app',
+    projeto: 'Buscador CEP'
+  },
+  {
+    imagem: `${ballebot}`,
+    link: 'https://ingridaquino.github.io/balle-bot/',
+    projeto: 'BalleBot'
+  }
+]
 
 export default class Responsive extends Component {
   render() {
@@ -55,48 +96,16 @@ export default class Responsive extends Component {
       <div className={styles.containerPro}>
         <h2> Projetos </h2>
         <Slider {...settings} className={styles.slider}>
-          <ComponentProject 
-            img={squirtle}
-            link={'https://squirtle-ong.netlify.app'}
-            projeto={'Squirtle'}     
-          />
 
-          <ComponentProject 
-              img={bookstan}
-              link={'https://book-stan.netlify.app/'}
-              projeto={'Bookstan'}      
-          />
-
-          <ComponentProject 
-              img={addRemove}
-              link={'https://github.com/Ingridaquino/add-remove'}
-              projeto={'ToDo List'}      
-          />
-
-          <ComponentProject 
-              img={ballebot}
-              link={'https://ingridaquino.github.io/balle-bot/'}
-              projeto={'BalleBot'}      
-          />
-
-          <ComponentProject 
-              img={buscador}
-              link={'https://frabjous-marzipan-5fc57e.netlify.app'}
-              projeto={'Buscador CEP'}      
-          />
-          
-          <ComponentProject 
-              img={tabuleiro}
-              link={'https://github.com/Ingridaquino/tabuleiro-next'}
-              projeto={'Tabuleiro'}      
-          />
-
-          
-          {/* <ComponentProject 
-              img={bookstan}
-              link={'https://book-stan.netlify.app/'}
-              projeto={'Matching-game'}      
-          /> */}
+          {
+            project.map((projects) => (
+              <ComponentProject  key={projects.projeto}
+              img={projects.imagem}
+              link={projects.link}
+              projeto={projects.projeto}     
+            />
+            ))
+          }
         </Slider>
       </div>
     );
